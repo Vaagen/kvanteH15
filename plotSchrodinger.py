4cc3714f84c2f4751fee3c7529ca5804f71fd40a
task123 <hakon.tasken@gmail.com>
2015-10-07 15:20:12 +0200

    testMerge

--- plotSchrodinger.py
+++ plotSchrodinger.py
@@ -1,13 +1,16 @@
 # this script plot simulations run by 'Schrodinger.h' and 'Schrodinger.cpp'
 # one only needs to specify the general filename common for all files produced in 'Schrodinger.h' and 'Schrodinger.cpp' (the filename argument in the fun member function)
+# as well as placing these files in the same directory as this file, 'plotSchroinger.py', or in a subdirectory of this directory
 
 fileName = "test_free_electron"
 
 # one should not be needing to do changes to the rest of the script
 
 import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib import animation
+import matplotlib
+matplotlib.use('TKAgg') # this is done to allow blit = True in FuncAnimation on mac
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import os
 
 # find location of file, 'name'
@@ -19,7 +22,7 @@ def find(name):
 # load variables used in simulation
 variableFile = open(find(fileName + "_variables.txt"), mode = 'r')
 line = variableFile.readline()
-numOfDim = float(line)
+numOfDim = int(line)
 line = variableFile.readline()
 Lx1 = float(line)
 line = variableFile.readline()
@@ -27,13 +30,13 @@ Lx2 = float(line)
 line = variableFile.readline()
 Lx3 = float(line)
 line = variableFile.readline()
-Nx1 = float(line)
+Nx1 = int(line)
 line = variableFile.readline()
-Nx2 = float(line)
+Nx2 = int(line)
 line = variableFile.readline()
-Nx3 = float(line)
+Nx3 = int(line)
 line = variableFile.readline()
-Nt = float(line)
+Nt = int(line)
 line = variableFile.readline()
 dx1 = float(line)
 line = variableFile.readline()
@@ -47,21 +50,21 @@ m = float(line)
 line = variableFile.readline()
 p = float(line)
 line = variableFile.readline()
-startX1 = float(line)
+startX1 = int(line)
 line = variableFile.readline()
-startX2 = float(line)
+startX2 = int(line)
 line = variableFile.readline()
-startX3 = float(line)
+startX3 = int(line)
 line = variableFile.readline()
 V0 = float(line)
 line = variableFile.readline()
 VThickness = float(line)
 line = variableFile.readline()
-situation = float(line)
+situation = int(line)
 line = variableFile.readline()
-potential = float(line)
+potential = int(line)
 line = variableFile.readline()
-probDistrb = float(line)
+probDistrb = int(line)
 line = variableFile.readline()
 SDx1 = float(line)
 line = variableFile.readline()
@@ -69,13 +72,13 @@ SDx2 = float(line)
 line = variableFile.readline()
 SDx3 = float(line)
 line = variableFile.readline()
-plotDensityX1 = float(line)
+plotDensityX1 = int(line)
 line = variableFile.readline()
-plotDensityX2 = float(line)
+plotDensityX2 = int(line)
 line = variableFile.readline()
-plotDensityX3 = float(line)
+plotDensityX3 = int(line)
 line = variableFile.readline()
-plotDensityT = float(line)
+plotDensityT = int(line)
 variableFile.close()
 
 # get data from plotFile
@@ -84,35 +87,44 @@ plotFile = np.fromfile(find(fileName + "_plot"), dtype=dt)
 if numOfDim == 1:
     plotFile = np.split(plotFile, 3) # only simulations for 1 dim store psi_r and psi_i in plotFile
 
+
 fig = plt.figure()
-ax = plt.subplot(1,1,1)
-plotData
-if numOfDim == 1:
-    plotData = ax.plot([],[],[],[],[],[])
-else if numOfDim == 2:
-    plotData = ax.contourf([], [], [], 500)
+ax = plt.axes(xlim=(0, Lx1), ylim=(0, 1))
+line, = ax.plot([], [], lw=2)
 
 # initialization function: plot the background of each frame
-def init():
+def init1D():
     line.set_data([], [])
     return line,
 
+def init2D():
+    #plotData = ax.contourf([], [], [], 500)
+    return
+
+def init3D():
+    return
+
 # animation function.  This is called sequentially
-def animate(i):
-    x = np.linspace(0, Lx1, Nx1)
-    y = plotFile[0][i]
-    line.set_data(x, y)
+x1 = np.linspace(0,Lx1,Nx1)
+def animate1D(i):
+    line.set_data(x1, plotFile[0][Nx1*(i):Nx1*(i+1)], 'r') #, x1, plotData[1][i], 'g', x1, plotData[2][i], 'k')
     return line,
 
+def animate2D(i):
+    return
 
+def animate3D(i):
+    return
 
-# First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure()
-ax = fig.add_subplot(111)
-line, = ax.plot([], [])
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(fig, animate, init, interval=20, blit=False)
+anim = None
+if numOfDim == 1:
+    anim = animation.FuncAnimation(fig, animate1D, init_func=init1D, frames = Nt, interval=20, blit=True)
+elif numOfDim == 2:
+    anim = animation.FuncAnimation(fig, animate2D, init_func=init2D, frames = Nt, interval=20, blit=True)
+elif numOfDim == 3:
+    anim = animation.FuncAnimation(fig, animate3D, init_func=init3D, frames = Nt, interval=20, blit=True)
 
 # save the animation as an mp4.  This requires ffmpeg or mencoder to be
 # installed.  The extra_args ensure that the x264 codec is used, so that
